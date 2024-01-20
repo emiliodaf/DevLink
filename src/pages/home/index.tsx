a3b9f0c4a1f14a00c198493f1ba92c377ed9fec8
Emílio de Aguiar <85713266+emiliodaf@users.noreply.github.com>
2024-01-20 19:08:39 -0300

    commit done

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 export function Home(){
     return(
-        <div>
+        <div className="flex flex-col w-full py-4 item-center justify-center">
             <h1>Pagina Home</h1>
         </div>
     )
