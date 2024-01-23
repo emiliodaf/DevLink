3adf2bf674dbab3e614acbd96e767b84dca76647
Emílio de Aguiar <85713266+emiliodaf@users.noreply.github.com>
2024-01-23 17:32:25 -0300

    commit done

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,7 @@
+import { Social } from '../../components/Social'
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
+
+
 export function Home(){
     return(
         <div className="flex flex-col w-full py-4 items-center justify-center">
@@ -5,13 +9,33 @@ export function Home(){
             <span className="text-grey-50 mb-5 mt-3">See my Links</span>
 
             <main className="flex flex-col w-11/12 max-w-xl text-center">
-        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none">
-            <a>
-                <p>
-                    Youtube Channel
-                </p>
-            </a>
-        </section>
+             <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+               <a>
+                  <p className="text-base md:text-lg ">
+                       Youtube Channel
+                  </p>
+               </a>
+             </section>
+             <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+               <a>
+                  <p className="text-base md:text-lg ">
+                       Facebook
+                  </p>
+               </a>
+             </section>
+            
+
+             <footer className="flex justify-center gap-3 my-4">
+               <Social url="https://www.youtube.com/">
+                  <FaYoutube size={35} color='#fff' />
+               </Social>
+               <Social url="https://www.facebook.com/">
+                  <FaFacebook size={35} color='#fff' />
+               </Social>
+               <Social url="https://www.instagram.com/">
+                  <FaInstagram size={35} color='#fff' />
+               </Social>
+             </footer>
 
             </main>
         </div>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom'
+import { Input } from '../../components/Input'
+
 export function Login(){
     return(
-        <div>
-            <h1>Pagina Login</h1>
+        <div className="flex flex-col w-full h-screen items-center justify-center">
+           <Link to="/">
+             <h1 className='mt-11 mb-7 text-white font-bold text-5xl'>Dev
+             <span className='bg-gradient-to-r from-yellow-500 to-pink-500 bg-clip-text text-transparent'>Link</span></h1>
+           </Link>
+          
+         <form className='w-full max-w-xl flex flex-col px-1'>
+           <Input/>
+         </form>
+
         </div>
     )
 
