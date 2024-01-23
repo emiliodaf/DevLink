fd4c57cf7a7597d36df935915b0a6b08c98253e3
Emílio de Aguiar <85713266+emiliodaf@users.noreply.github.com>
2024-01-23 16:04:07 -0300

    commit now

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,19 @@
 export function Home(){
     return(
-        <div className="flex flex-col w-full py-4 item-center justify-center">
-            <h1>Pagina Home</h1>
+        <div className="flex flex-col w-full py-4 items-center justify-center">
+            <h1 className='md:text-4xl text-3xl font-bold text-white mt-20'>Dev Link Tree Home</h1>
+            <span className="text-grey-50 mb-5 mt-3">See my Links</span>
+
+            <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none">
+            <a>
+                <p>
+                    Youtube Channel
+                </p>
+            </a>
+        </section>
+
+            </main>
         </div>
     )
 
